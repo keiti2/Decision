5f6765d7612edbb834852905f92302e6ab0ba800
keiti2 <39093612+keiti2@users.noreply.github.com>
2019-04-15 20:02:15 -0300

    Inicio
    Preparando para mvp1

---
+++ logicaInterface.js
@@ -0,0 +1,204 @@
+var variaveis, restricoes, iteracoes, tbody, resultado, header, objetivo, restricoesOperadores = [], simplexTabela; // Variáveis com a referência dos elementos da página
+function gerarTabela() {
+    tbody = document.getElementById("tabela_restricoes");
+    tbody.innerHTML = "";
+    var linha = document.getElementById("linha_funcao");
+    objetivo = $("#objetivo > input:checked").first().val();
+    var inner = "";
+    var linhatexto = "<tr><th>" + (objetivo.toUpperCase()) + " Z = </th>";
+    variaveis = document.getElementById("variaveis").value;
+    restricoes = document.getElementById("restricoes").value;
+    iteracoes = document.getElementById("iteracoes").value;
+    restricoesOperadores = [];
+    if (!restricoes || !variaveis || isNaN(restricoes) || isNaN(restricoes)) {
+        alert('Número de variáveis ou restrições inválidos.')
+        variaveis.value = null;
+        restricoes.value = null;
+        $("#parametros").hide();
+        $("#reiniciar").hide();
+        return null;
+    }
+    for (let index = 0; index < restricoes; index++) {
+        restricoesOperadores.push("<=");
+        inner += "<tr>"
+        for (let variavel = 0; variavel < variaveis; variavel++) {
+            var id = index + "_" + variavel;
+            if (!index) {
+                linhatexto += '<td>\
+                <div class="input-group-prepend"  style="max-width: 150px">\
+                    <input type="number" step="0.01" id="'+ linha + variavel + '" class="input_linha" style="width: 50px" ><div class="input-group-text">x' + (variavel + 1) + '</div>\
+                </div></td>';
+
+            }
+            inner += '<td>\
+                <div class="input-group-prepend"  style="max-width: 150px">\
+                    <input type="number" step="0.01" id="'+ id + '" class="input_simplex" style="width: 50px" ><div class="input-group-text">x' + (variavel + 1) + '</div>\
+                </div></td>';
+        }
+        inner += '<td><div class="input-group-prepend" style="max-width: 150px">\
+                <div class="input-group-text">&ange;</div><input id="resultado_'+ index + '" class="input_simplex" type="number" step="0.01" style="width: 50px">\
+                </div></td></tr>';
+
+    }
+    linhatexto += "</tr>";
+    tbody.innerHTML = inner;
+
+    linha.innerHTML = linhatexto;
+    $("#inicio").hide();
+    $("#parametros").show();
+}
+
+function reiniciar() {
+    $("#parametros").hide();
+    $("#solucao").hide();
+    $("#passoapasso").hide();
+    $("#reiniciar").hide();
+    $("#inicio").show();
+
+    document.getElementById("variaveis").value = null;
+    document.getElementById("restricoes").value = null;
+}
+
+function resolverSimplex() {
+    var inputs = document.getElementsByClassName("input_simplex");
+    var linha = document.getElementsByClassName("input_linha");
+    var matriz = [];
+    var cabecalho = [0];
+    for (let i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value || isNaN(inputs[i].value)) {
+            alert('Variáveis inválidas.')
+            $("#solucao").hide();
+            $("#reiniciar").hide();
+            return null
+        }
+        var indices = inputs[i].id.split("_");
+        matriz[indices[0]] = matriz[indices[0]] || [];
+        matriz[indices[0]][indices[1]] = Number(inputs[i].value);
+    }
+
+    for (let i = 0; i < linha.length; i++) {
+        if (!linha[i].value || isNaN(inputs[i].value)) {
+            alert('Função inválida.')
+            $("#solucao").hide();
+            $("#reiniciar").hide();
+            return null
+        }
+        cabecalho.push(Number(linha[i].value) * -1);
+    }
+
+    var tabelaSimplex = [];
+    tabelaSimplex.push(cabecalho);
+
+    for (let i = 0; i < matriz.length; i++) {
+        var x = [matriz["resultado"][i]].concat(matriz[i]);
+        tabelaSimplex.push(x);
+    }
+
+    console.log(tabelaSimplex);
+
+    simplexTabela = {
+        m: tabelaSimplex.length,
+        n: tabelaSimplex[0].length,
+        tableau: tabelaSimplex,
+        restricoes: restricoesOperadores,
+        max: objetivo == "max"
+    };
+
+    if (!simplex(simplexTabela, iteracoes)) {
+        return alert("Não foi possível achar uma solução.");
+    }
+
+    console.log(simplexTabela);
+
+    resultado = document.getElementById("resultado_simplex");
+    header = document.getElementById("resultado_header");
+
+    var resultTabela = gerarTabelaSolucao(simplexTabela);
+
+    resultado.innerHTML = resultTabela.resultado;
+    header.innerHTML = resultTabela.header;
+    $("#passoapassotitle").html("Passo a passo ["+ simplexTabela.iteracoes + " iteração(ões)]" );
+    avancar();
+    $("#solucao").show();
+    $("#passoapasso").show();
+    $("#reiniciar").show();
+    $("html, body").animate({
+        scrollTop: 1000
+    }, 1000);
+}
+
+var pag = 0;
+
+function avancar(i) {
+    var tabelaPassoAPasso = $("#passoapasso");
+    pag += i ? i : 0;
+    if (!pag) {
+        $("#anterior").hide();
+    } else {
+        $("#anterior").show();
+        if (pag == (simplexTabela.passoapasso.length - 1)) {
+            $("#proximo").hide();
+        } else {
+            $("#proximo").show();
+        }
+    }
+
+    var htmlTabela = gerarTabelaSolucao(simplexTabela.passoapasso[pag]);
+    tabelaPassoAPasso.find("thead").html(htmlTabela.header);
+    tabelaPassoAPasso.find("tbody").html(htmlTabela.resultado);
+}
+
+function gerarTabelaSolucao(tabela) {
+    var inner = "";
+    var linhatexto = "<tr><th></th>";
+
+    var Z = "<tr><th>Z</th>";
+    for (let index = 1; index < tabela.m; index++) {
+        inner += "<tr><td>" + tabela.labelColumn[index - 1] + "</td>";
+        for (let variavel = 0; variavel < tabela.n; variavel++) {
+            let id = index + "_" + variavel;
+            let value = Number.isInteger(tabela.tableau[index][variavel]) ? tabela.tableau[index][variavel] : tabela.tableau[index][variavel].toFixed(3);
+            inner += '<td>' + value + '</td>';
+        }
+        inner += '</tr>';
+    }
+    for (let variavel = 0; variavel < tabela.n; variavel++) {
+        var nome;
+        Z += '<th>' + Number(tabela.tableau[0][variavel]).toFixed(3) + '</th>';
+        linhatexto += '<th>' + (variavel ? tabela.labelRow[variavel - 1] : "b") + '</th>';
+    }
+    Z += "</tr>";
+    linhatexto += "</tr>";
+
+
+    return { resultado: inner + Z, header: linhatexto };
+}
+
+function analise() {
+    var x = getSensibilityTable(simplexTabela);
+    var tabela = $("#table-analise");
+    var thead = "<thead><tr>"
+    for (let index = 0; index < x.labelRow.length; index++) {
+        const element = x.labelRow[index];
+        thead += "<th>" + element + "</th>";
+    }
+    thead += "</tr></thead>";
+    var tbody = "<tbody>";
+    for (let index = 0; index < x.labelColumn.length; index++) {
+        tbody += "<tr><th>" + x.labelColumn[index] + "</th>";
+        for (let variavel = 0; variavel < 4; variavel++) {
+            tbody += "<td>" + (isNaN(Number(x.table[index][variavel])) ? x.table[index][variavel] : x.table[index][variavel].toFixed(3)) + "</td>";
+        }
+        tbody += '</tr>';
+    }
+
+    tbody += "</tbody>";
+    tabela.html(thead + tbody);
+
+    $("#analise").show();
+}
+$(document).ready(function(){
+    $("#fechar").on('click', function () { 
+        $("#analise").hide(); 
+    });
+})
---
+++ simplex.js
@@ -0,0 +1,253 @@
+function Matriz(x, y) {
+    let clona = function () { return this.slice(); };
+    return Array.apply(null, Array(x)).map(clona, Array.apply(null, Array(y)).map(Number.prototype.valueOf, 0));
+}
+
+class Tableau {
+    constructor(m, n) {
+        this.m = Number(m) || 0;
+        this.n = Number(n) || 0;
+        this.max = true;
+        this.labelColumn = [];
+        this.labelRow = [];
+        this.createCopy = function () { return createCopy(this); };
+        this.restricoes = Array.apply(null, Array(m)).map(Number.prototype.valueOf, 0);
+        this.tableau = Matriz(this.m, this.n);
+    }
+}
+
+function pivot_on(tab, row, col) {
+    let i, j, pivot, a, multiplier;
+    pivot = tab.tableau[row][col];
+    console.log("Encontrou pivot: " + pivot, " linha: ", row, " coluna: ", col);
+    tab.labelColumn[row - 1] = tab.labelRow[col - 1];
+
+    for (j = 0; j < tab.n; j++)
+        tab.tableau[row][j] /= pivot;
+    for (i = 0; i < tab.m; i++) {
+        multiplier = tab.tableau[i][col];
+        if (i == row) continue;
+        for (j = 0; j < tab.n; j++) {
+            tab.tableau[i][j] -= multiplier * tab.tableau[row][j];
+        }
+    }
+}
+
+function find_pivot_column(tab) {
+    let j, pivot_col = 1;
+    let lowest = tab.tableau[0][pivot_col];
+    for (j = 1; j < tab.n; j++) {
+        if (tab.tableau[0][j] < lowest) {
+            lowest = tab.tableau[0][j];
+            pivot_col = j;
+        }
+    } if (lowest >= 0) {
+        return -1;
+    }
+    return pivot_col;
+}
+
+function find_pivot_row(tab, pivot_col) {
+    let i, pivot_row = 0, min_ratio = -1, ratio;
+    for (i = 1; i < tab.m; i++) {
+        if (tab.tableau[i][pivot_col]) {
+            ratio = tab.tableau[i][0] / (tab.tableau[i][pivot_col]);
+            if ((ratio > 0 && ratio < min_ratio) || min_ratio < 0) {
+                min_ratio = ratio;
+                pivot_row = i;
+            }
+        }
+    }
+    return min_ratio == -1 ? -1 : pivot_row;
+}
+function add_variaveis_nao_basicas(tab) {
+    let i, j;
+    let rest = 0;
+    tab.labelColumn = []; // Cria a coluna para a label das linhas da tabela, com exceção da linha Z
+
+    for (let index = 0; index < tab.m - 1; index++) {
+        rest += tab.restricoes[index] == "<=" ? 1 : 2;
+    }
+
+    for (i = 0; i < tab.m; i++) {
+        tab.labelColumn.push("f" + (i + 1));
+        for (j = 1; j < tab.m; j++) {
+            tab.tableau[i][j + tab.n - 1] = (i == j) ? (tab.restricoes[i - 1] == "<=" ? 1 : -1) : 0;
+        }
+    }
+
+    tab.labelColumn.pop(); // Remove a linha adicionada a mais (não conta a linha Z)
+
+    for (i = 1, l = tab.labelColumn.length; i < tab.m; i++) {
+        if (tab.restricoes[i - 1] == ">=") {
+            tab.hasExtraVariable = true;
+            tab.labelColumn.push("f" + (tab.labelColumn.length + 1));
+            for (j = 0; j < tab.m; j++) {
+                tab.tableau[j].push((i == j) ? 1 : 0);
+            }
+        }
+    }
+
+    tab.labelRow = []; // cria a linha cabeçalho da matriz, sendo a primeira coluna b
+    for (i = 1; i < tab.n; i++) { // começa a iterar as variáveis básicas
+        tab.labelRow.push("x" + i);
+    }
+    tab.labelRow = tab.labelRow.concat(tab.labelColumn); // concatena as variáveis básicas com as variáveis não básicas
+    tab.n = tab.labelRow.length + 1;
+}
+
+function calculateZLine(tab, final) {
+    var newZ = [];
+    if (!final) {
+        for (i = 0; i < tab.n; i++) {
+            newZ.push(0);
+            if (i && tab.labelRow[i - 1].replace("f", "") > (tab.m - 1)) {
+                continue;
+            }
+            for (j = 1; j < tab.m; j++) {
+                if (tab.restricoes[j - 1] == ">=") {
+                    newZ[i] -= tab.tableau[j][i];
+                }
+            }
+        }
+    }
+    else {
+        newZ.push(0);
+        for (j = 1; j < tab.m; j++) {
+            let x = tab.labelColumn[j - 1].match(/^(?:x)(\d+)$/);
+            if (x) {
+                newZ[0] += tab.funcaoObjetivo[Number(x[1])] * tab.tableau[j][0];
+            }
+        }
+        for (i = 1; i < tab.n; i++) {
+            newZ.push(0);
+            for (j = 1; j < tab.m; j++) {
+                let x = tab.labelColumn[j - 1].match(/^(?:x)(\d+)$/);
+                newZ[i] += ((tab.funcaoObjetivo[Number(x ? x[1] : -1)] || 0) * tab.tableau[j][i] || 0) - ((x ? tab.funcaoObjetivo[i] : 0) || 0);
+            }
+        }
+    }
+
+    tab.tableau[0] = newZ;
+}
+
+function criarFuncaoObjetivo(tab) {
+    tab.funcaoObjetivo = tab.tableau[0].slice();
+}
+
+function simplex(tab, iteracoes) {
+    console.log("TABLEAU INICIAL: ", JSON.stringify(tab));
+    let loop = 0;
+    iteracoes = Number(iteracoes) || 20;
+    try {
+        let passoapasso = [];
+        let pivot_col, pivot_row;
+        criarFuncaoObjetivo(tab);
+        add_variaveis_nao_basicas(tab);
+        passoapasso.push(createCopy(tab));
+        do {
+            if(loop > 20) return false;
+            if (loop >= iteracoes) break;
+
+            if (!tab.max) {
+                if (!loop && tab.hasExtraVariable) calculateZLine(tab);
+                pivot_col = find_pivot_column(tab);
+                if (pivot_col < 0) {
+                    tab.max = true;
+                    calculateZLine(tab, true);
+                    break;
+                }
+                pivot_row = find_pivot_row(tab, pivot_col);
+                if (pivot_row < 0) {
+                    tab.max = true;
+                    if (tab.hasExtraVariable) calculateZLine(tab, true);
+                    break;
+                }
+                pivot_on(tab, pivot_row, pivot_col);
+            }
+            else {
+                pivot_col = find_pivot_column(tab);
+                if (pivot_col < 0) {
+                    break;
+                }
+                pivot_row = find_pivot_row(tab, pivot_col);
+                if (pivot_row < 0) {
+                    break;
+                }
+                pivot_on(tab, pivot_row, pivot_col);
+            }
+            passoapasso.push(createCopy(tab));
+        } while (++loop);
+        tab.passoapasso = passoapasso;
+        tab.iteracoes = loop;
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+function createCopy(tableau) {
+    let copy = new Tableau(tableau.m, tableau.n);
+    for (let i = 0; i < tableau.tableau.length; i++) {
+        copy.tableau[i] = tableau.tableau[i].slice();
+    }
+    copy.funcaoObjetivo = tableau.funcaoObjetivo.slice();
+    copy.restricoes = tableau.restricoes.slice();
+    copy.labelColumn = tableau.labelColumn.slice();
+    copy.labelRow = tableau.labelRow.slice();
+    return copy;
+}
+
+function getSensibilityTable(final) {
+    var sensibilityTable = {
+        labelRow: ["Variável", "Valor Final", "Preço Sombra", "+", "-"],
+        labelColumn: final.labelRow.concat(["Z"]),
+        table: Matriz(final.labelRow.length + 1, 4)
+    };
+    // VALOR FINAL
+    for (let index = 0; index <= final.labelColumn.length; index++) {
+        let i = sensibilityTable.labelColumn.indexOf(final.labelColumn[index]);
+        sensibilityTable.table[i >= 0 ? i : (sensibilityTable.labelColumn.length - 1)][0] = (final.tableau[index + 1] || final.tableau[0])[0];
+    }
+    // PREÇO SOMBRA
+    for (let index = 0; index < sensibilityTable.labelColumn.length; index++) {
+        sensibilityTable.table[index][1] = "-";
+        if (sensibilityTable.labelColumn[index].match(/^f/)) {
+            sensibilityTable.table[index][1] = final.tableau[0][(index + 1) % sensibilityTable.labelColumn.length];
+        }
+    }
+    // Calcular + e - 
+    let firstColumn = final.labelRow.indexOf("f1") + 1;
+    for (let index = 0, total = sensibilityTable.labelColumn.length; index < total; index++) {
+        sensibilityTable.table[index][2] = "-";
+        sensibilityTable.table[index][3] = "-";
+
+        if (index >= firstColumn && (total) > index) {
+            let divide = final.tableau[1][0];
+            let maior = (divide) / (final.tableau[1][index]), menor = (divide) / (final.tableau[1][index]);
+
+            for (let l = 1; l <= final.labelColumn.length; l++) {
+                let divide = final.tableau[l][0];
+                const element = final.tableau[l][index];
+                let mn = (divide) / (element), mx = (divide) / (element);
+                if (Math.abs(mn) !== Infinity) {
+                    if (mn < menor) {
+                        menor = mn;
+                    }
+                    if (mx > maior) {
+                        maior = mx;
+                    }
+                }
+            }
+            sensibilityTable.table[index - 1][2] = Math.abs(maior);
+            sensibilityTable.table[index - 1][3] = Math.abs(menor);
+            if (final.labelColumn.indexOf(final.labelRow[index]) >= 0){
+                sensibilityTable.table[index - 1][2] = Math.abs(menor);
+                sensibilityTable.table[index - 1][3] = Math.abs(maior);
+            }
+        }
+
+    }
+    return sensibilityTable;
+}
