7e3aa1609fcf111c286c62e3895a7aad85a6b1bd
keiti2 <39093612+keiti2@users.noreply.github.com>
2019-04-16 16:20:54 -0300

    Alteração Gera PDf
    Incluso geração de pdf solucao direta.

--- simplex.js
+++ simplex.js
@@ -251,3 +251,27 @@ function getSensibilityTable(final) {
     }
     return sensibilityTable;
 }
+
+//gerar pdf
+$(document).ready(function(){
+    $('#btnPDF').click(function() {
+      savePDF(document.querySelector('#solucao'));
+    });
+});
+  
+function savePDF(codigoHTML) {
+  var doc = new jsPDF('portrait', 'pt', 'a4'),
+      data = new Date();
+  margins = {
+    top: 40,
+    bottom: 60,
+    left: 40,
+    width: 1000
+  };
+  doc.fromHTML(codigoHTML,
+               margins.left, // x coord
+               margins.top, { pagesplit: true },
+               function(dispose){
+    doc.save("Relatorio - "+data.getDate()+"/"+data.getMonth()+"/"+data.getFullYear()+".pdf");
+  });
+}
